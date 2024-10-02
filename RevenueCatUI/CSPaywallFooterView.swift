e9ade4f1a8090048286165c4c184deeb54e29d43
Adam <aoverholtzer@users.noreply.github.com>
2024-10-02 10:43:06 -0700

    mark unavail on watchos

--- RevenueCatUI/CSPaywallFooterView.swift
+++ RevenueCatUI/CSPaywallFooterView.swift
@@ -9,11 +9,12 @@
 import RevenueCat
 import SwiftUI
 
-#if !os(macOS) && !os(tvOS)
+#if !os(macOS) && !os(tvOS) && !os(watchOS)
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
 @available(macOS, unavailable, message: "RevenueCatUI does not support macOS yet")
 @available(tvOS, unavailable, message: "RevenueCatUI does not support tvOS yet")
+@available(watchOS, unavailable, message: "Footers not supported on watchOS")
 public struct CSPaywallFooterView: View {
     let offering: Offering?
     let condensed: Bool
